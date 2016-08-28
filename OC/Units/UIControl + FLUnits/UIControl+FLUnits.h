58e9bdac9f807aa9d764b28629e85795e119c911
孔凡列 <kongfanlie@clarence-2.local>
2016-08-28 23:36:24 +0800

    UIControl + FLUnits

---
+++ OC/Units/UIControl + FLUnits/UIControl+FLUnits.h
@@ -0,0 +1,15 @@
+//
+//  UIControl+FLUnits.h
+//  test
+//
+//  Created by 孔凡列 on 16/8/28.
+//  Copyright © 2016年 clarence. All rights reserved.
+//  原文出处：http://www.jianshu.com/p/65ce6471cd0f
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (FLUnits)
+
+@property (nonatomic, assign) NSTimeInterval custom_acceptEventInterval;// 可以用这个给重复点击加间隔
+
+@end
